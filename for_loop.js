dd2ca99b30d5dbe5badc0122e17e425c86669dfd
Jitumanicit <34137570+Jitumanicit@users.noreply.github.com>
2019-03-09 02:15:31 +0530

    for loop

---
+++ for_loop.js
@@ -0,0 +1,16 @@
+const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat']
+// console.log(days.length);
+// Reverse order
+for (let index = days.length - 1; index >= 0; index--) {
+  // const element = days[index];
+  // console.log(element);
+  //insetend of this
+  console.log(days[index]);
+}
+// Straight Order
+for (let index = 0; index < days.length ; index++) {
+  // const element = days[index];
+  // console.log(element);
+  //insetend of this
+  console.log(days[index]);
+}
